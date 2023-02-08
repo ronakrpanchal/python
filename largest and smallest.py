de51d2f7be8b7a0352527dd5ba07bd069771ac80
ronakrpanchal <124129862+ronakrpanchal@users.noreply.github.com>
2023-02-08 11:50:53 +0530

    Add files via upload

---
+++ largest and smallest.py
@@ -0,0 +1,11 @@
+def ls2():
+    a=int(input("a= "))
+    b=int(input("b= "))
+    if a>b:
+        print("a is greater than b")
+    elif b>a:
+        print("b is greater than a")
+    else:
+        print("both are same")
+ls2()
+        
